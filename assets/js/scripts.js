465ab6a7bc1e33e816c0e9773d21b06bf9d1696e
vietthai.3011 <vietthai116@example.com>
2024-05-12 09:17:59 +0700

    Làm xong trang checkout, shipping, ...

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -189,3 +189,24 @@ window.addEventListener("template-loaded", () => {
         };
     });
 });
+window.addEventListener("template-loaded", () => {
+    const tabsSelector = "product-tab__item";
+    const contentsSelector = "product-tab__content";
+
+    const tabActive = `${tabsSelector}--current`;
+    const contentActive = `${contentsSelector}--current`;
+
+    const tabContainers = $$(".js-tabs");
+    tabContainers.forEach((tabContainer) => {
+        const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+        const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+        tabs.forEach((tab, index) => {
+            tab.onclick = () => {
+                tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
+                tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
+                tab.classList.add(tabActive);
+                contents[index].classList.add(contentActive);
+            };
+        });
+    });
+});
